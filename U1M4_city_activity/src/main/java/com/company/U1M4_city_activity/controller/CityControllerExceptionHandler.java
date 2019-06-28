02d29f4a065e97b84917afd8d1f146bb1ca3c866
unknown <megrumble@gmail.com>
2019-06-28 13:53:21 -0400

    added exception handler class

---
+++ U1M4_city_activity/src/main/java/com/company/U1M4_city_activity/controller/CityControllerExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.company.U1M4_city_activity.controller;
+
+import org.omg.CosNaming.NamingContextPackage.NotFound;
+import org.springframework.hateoas.VndErrors;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+@RequestMapping(produces = "application/vnd.error+json")
+public class CityControllerExceptionHandler {
+
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> cityValidationError(MethodArgumentNotValidException e, WebRequest request){
+        //BindingResult holds the validation errors
+        BindingResult result = e.getBindingResult();
+
+        //Validation errors are stored in the FieldErrors objects
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        //Translate the FieldErrors to VndErrors
+        List<VndErrors.VndError> vndErrorList = new ArrayList<>();
+        for(FieldError fieldError: fieldErrors){
+            VndErrors.VndError vndError = new VndErrors.VndError(request.toString(), fieldError.getDefaultMessage());
+
+            vndErrorList.add(vndError);
+        }
+
+        //Wrap all the VndError objects in a VndError object
+        VndErrors vndErrors = new VndErrors(vndErrorList);
+
+        //Create and return responseEntity
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(vndErrors, HttpStatus.UNPROCESSABLE_ENTITY);
+
+        return responseEntity;
+    }
+    @ExceptionHandler(value = {IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    public ResponseEntity<VndErrors> invalidInputError(IllegalArgumentException e, WebRequest request){
+
+        VndErrors vndError = new VndErrors(request.toString(), e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(vndError, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+    @ExceptionHandler(value = {NotFound.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<VndErrors> numberNotFound(NotFound e, WebRequest request){
+        VndErrors vndError = new VndErrors(request.toString(), e.getMessage());
+        ResponseEntity<VndErrors> responseEntity = new ResponseEntity<>(vndError, HttpStatus.UNPROCESSABLE_ENTITY);
+        return responseEntity;
+    }
+
+}
